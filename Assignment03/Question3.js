7641f11c59974ed2b0898c8b7760b5e2cb081c17
Rajesh Subedi <rsubedi@mum.edu>
2018-01-18 18:43:20 -0600

    Assignment03

--- Assignment03/Question3.js
+++ Assignment03/Question3.js
@@ -1,18 +1,9 @@
 
-
 'use strict'
 
-//var util = require('util');
-//EventEmitter = require('events').EventEmitter;
 
 const EventEmitter = require('events');
 
-/*var Clock = function(){
-    var self = this;
-    setInterval(function(){
-        self.emit('woohoo');
-    },1000);
-};*/
 
 class Clock extends EventEmitter{
 	constructor(){
@@ -29,22 +20,6 @@ class Clock extends EventEmitter{
 }
 
 
-/*class Clock extends EventEmitter {
-    constructor() {
-        super();
-        this.tickEv();
-    }
-    tickEv() {
-        const scope = this;
-        
-        setInterval(function() {
-            scope.emit('tick');
-        }, 1000); // 1 sec interval
-    }
-}*/
-
-
-// var util = require('util');
 var ticker = new Clock();
 ticker.on("tick",function(){
     console.log("woohoo");
